b99640024a011bfb8b9361c9244ca85451207d8d
Margaret Donin <rtdonin@gmail.com>
2020-04-14 12:07:20 -0400

    Java Code-Along: Window Master

---
+++ NetBeansProjects/classwork/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+Created by: Margaret Donin
+Date created: 04/13/20
+Date revised:
+*/
+package rtdonin.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
