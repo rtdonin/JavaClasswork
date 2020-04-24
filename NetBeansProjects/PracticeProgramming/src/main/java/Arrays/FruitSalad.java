94f512db9c2f4c1ad068643fa2f12c2510af1f6c
Margaret Donin <rtdonin@gmail.com>
2020-04-24 14:42:37 -0400

    just updating the repository with everything

--- NetBeansProjects/PracticeProgramming/src/main/java/Arrays/FruitSalad.java
+++ NetBeansProjects/PracticeProgramming/src/main/java/Arrays/FruitSalad.java
@@ -1,12 +1,12 @@
 /*
 Created by: Margaret Donin
 Date created: 04/20/20
-Date revised:
+Date revised: 04/24/20
+
+I went big, but needed to go home instead. Wasn't ready to do it the way I wanted to.
 */
 
 package Arrays;
-
-import java.util.regex.*;
 
 public class FruitSalad {
     public static void main(String[] args) {
@@ -15,46 +15,55 @@ public class FruitSalad {
                 "Raspberry", "Blood Orange", "Sungold Tomato", "Fuji Apple", "Blackberry", "Banana", "Pineapple",
                 "Florida Orange", "Kiku Apple", "Mango", "Satsuma Orange", "Watermelon", "Snozzberry" };
 
-        String[] fruitSalad;
+        String[] fruitSalad = new String [12];
 
         // Code Recipe for fruit salad should go here!
-        int indexOfSalad = 0;
-        int numKindsOfBerries = 0;
-        int numKindsOfApples = 0;
-        int numKindsOfOrange = 0;
-        int numKindsOfTomato = 0;
+        int numKindOfBerry = 0;
+        int numKindOfApple = 0;
+        int numKindOfOrange = 0;
+        int numKindOfOther = 0;
+        int fruitIndex = 0;
 
-
-        System.out.println("Total list of fruits in salad: ");
-        displayArray(fruitSalad);
-        System.out.println("Number of fruits in salad:");
-        System.out.println("There are " + numKindsOfBerries + " berries.");
-        System.out.println("There are " + numKindsOfApples + " apples.");
-        System.out.println("There are " + numKindsOfOrange + " oranges.");
-        System.out.println("There are " + numKindsOfTomato + " tomatos.");
-    }
-    
-    public static int[] matchToPattern(String pattern, String[] array) {
-        String[] tempArray;
-        // create patterns object
-        Pattern fruitPattern = Pattern.compile(pattern);
-
-        for (int i = 0; i < array.length; i++) {
-
-            // create matcher objmatcherect
-            Matcher fruitMatch = fruitPattern.matcher();
-
-            if (fruitMatch.matches()) {
-                
+        for (int i = 0; i < fruit.length && fruitIndex < fruitSalad.length; i++) {
+            if (fruit[i].contains("berry")) {
+                fruitSalad[fruitIndex] = fruit[i];
+                numKindOfBerry++;
+                fruitIndex++;
             }
         }
         
-        return tempArray
-    }
-    
-    public static void displayArray(String[] array){
-        for (String element : array){
-            System.out.print(element + " ");
+        for (int i = 0; i < fruit.length && fruitIndex < fruitSalad.length; i++) {
+            if (fruit[i].contains("Apple") && numKindOfApple < 3) {
+                fruitSalad[fruitIndex] = fruit[i];
+                numKindOfApple++;
+                fruitIndex++;
+                
+            } else if (fruit[i].contains("Orange") && numKindOfOrange < 2) {
+                fruitSalad[fruitIndex] = fruit[i];
+                numKindOfOrange++;
+                fruitIndex++;
+
+            } else if (!fruit[i].contains("Tomato") && !fruit[i].contains("berry")
+                    && !fruit[i].contains("Apple") && !fruit[i].contains("Orange")){
+                fruitSalad[fruitIndex] = fruit[i];
+                numKindOfOther++;
+                fruitIndex++;
+            }
+                
         }
+        System.out.println("Total list of fruits in salad: ");
+        
+        for (int i = 1; i < fruitSalad.length; i++){
+            System.out.print(fruitSalad[i-1] + " ");
+            if (i % 4 == 0){
+                System.out.println();
+            }
+        }
+        System.out.println();
+        System.out.println("Number of fruits in salad:");
+        System.out.println("There are " + numKindOfBerry + " berries.");
+        System.out.println("There are " + numKindOfApple + " apples.");
+        System.out.println("There are " + numKindOfOrange + " oranges.");
+        System.out.println("There are " + numKindOfOther + " other fruit.");
     }
 }
