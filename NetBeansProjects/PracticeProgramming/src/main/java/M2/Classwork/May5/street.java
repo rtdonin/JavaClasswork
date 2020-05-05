44bc515099bcfdab1848c1ecd5d08233d08d22ec
Margaret Donin <rtdonin@gmail.com>
2020-05-05 13:30:53 -0400

    updated classwork

--- NetBeansProjects/PracticeProgramming/src/main/java/M2/Classwork/May5/street.java
+++ NetBeansProjects/PracticeProgramming/src/main/java/M2/Classwork/May5/street.java
@@ -7,10 +7,15 @@ package M2.Classwork.May5;
 public class street{
     public static void main(String[] args) {
         wandStore olivanders = new wandStore("Olivanders", "Olivander", 20);
+        Store flourishAndBlottsBookseller = new Store("Flourish and Blotts Bookseller", "Mr. Manager", 50);
         
         olivanders.enter();
         olivanders.findWand();
+        olivanders.sell();
+        olivanders.exit();
         
-        
+        flourishAndBlottsBookseller.enter();
+        flourishAndBlottsBookseller.sell();
+        flourishAndBlottsBookseller.exit();
     }
 }
